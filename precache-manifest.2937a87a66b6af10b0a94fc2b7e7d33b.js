f3b42d09bdf6c11a895aece8abfbd72647029205
hsh919 <m.shifu7@outlook.com>
2020-08-27 22:29:30 +0800

    first commit

---
+++ precache-manifest.2937a87a66b6af10b0a94fc2b7e7d33b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8f8c111ea7257ded368691f9adf87d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a7c8291ee28be3865b",
+    "url": "/static/css/main.0fb8687e.chunk.css"
+  },
+  {
+    "revision": "4526b2378c80d4f95883",
+    "url": "/static/js/2.df6e7efa.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/static/js/2.df6e7efa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a7c8291ee28be3865b",
+    "url": "/static/js/main.e71ea35d.chunk.js"
+  },
+  {
+    "revision": "f073214ecb8a8deed1db",
+    "url": "/static/js/runtime-main.9a7a4541.js"
+  }
+]);
